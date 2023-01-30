44665cf29b95677ec1a4f868ff76a97c0fa47bdc
ltt02 <lytantai02@gmail.com>
2023-01-30 21:57:49 +0700

    Refactor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP Logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -17,14 +24,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource\\views'));
 
-// route
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// Home, search, contact -> site.js
 
-app.get('/news', (req, res) => {
-    res.render('news');
-});
+// Route init
+route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+function route(app) {
+    // route
+    app.use('/news', newsRouter);
+    
+    app.use('/', siteRouter);
+    // Local host --- Hosting
+
+    // Action ---> Dispatcher ---> Function handler
+}
+
+module.exports = route;
